4ee6de59b49e69260ed59090686bc8d6ae138b3d
Caleb Nasio <nascal3@gmail.com>
2024-10-25 09:04:12 +0300

    Added use of environment variables

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -2,6 +2,7 @@ import {Injectable, signal} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import { filterFoodResults, food } from './results.interface';
 import {BehaviorSubject, catchError, Observable, of} from 'rxjs';
+import {environment} from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +17,9 @@ export class FilterService {
   constructor(private http: HttpClient) {}
 
   filterFood(food: string) {
-    const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
-    const API_KEY = 'ae9acc90cbe842faa02e9cc3b209ac51'
-    this.http.get<any>(`${baseUrl}?apiKey=${API_KEY}&query=${food}`)
+    // const baseUrl = 'https://api.spoonacular.com/recipes/complexSearch'
+    // const API_KEY = 'ae9acc90cbe842faa02e9cc3b209ac51'
+    this.http.get<any>(`${environment.apiUrl}/complexSearch?apiKey=${environment.API_KEY}&query=${food}`)
       .pipe(
         catchError((error) => {
           console.error('Failed to fetch posts', error)
@@ -31,10 +32,10 @@ export class FilterService {
   }
 
   getFoodDetails(id: number) {
-    const baseUrl = 'https://api.spoonacular.com/recipes/'
-    const API_KEY = 'ae9acc90cbe842faa02e9cc3b209ac51'
+    // const baseUrl = 'https://api.spoonacular.com/recipes/'
+    // const API_KEY = 'ae9acc90cbe842faa02e9cc3b209ac51'
 
-    const url = `${baseUrl}${id}/information?apiKey=${API_KEY}`
+    const url = `${environment.apiUrl}/${id}/information?apiKey=${environment.API_KEY}`
     this.http.get<any>(url)
       .pipe(
         catchError((error) => {
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: 'https://api.spoonacular.com/recipes',
+  API_KEY: 'ae9acc90cbe842faa02e9cc3b209ac51'
+};
